e42ba66461cab9c7f4d4c5539aedacecb59e970e
Vladka <vlad.lymar@list.ru>
2022-08-26 11:43:44 +0500

    Создана таблица с 50 элементами

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -17,3 +17,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        //получаем экземпляр ячейки
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "contactCellIdentifier")
+        //конфигурируем ячейку
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+        
+        //возвращаем сконфигурированный экземпляр ячейки
+        return cell
+    }
+    
+    
+    
+}
