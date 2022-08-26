9d5623d978cbd39e877c6fc09982780d9e59fc41
Vladka <vlad.lymar@list.ru>
2022-08-26 12:00:59 +0500

    Создан метод, позволяющий корретно отобажать данные в переиспользуемых ячейках

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -26,15 +26,26 @@ extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         
-        //получаем экземпляр ячейки
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "contactCellIdentifier")
-        //конфигурируем ячейку
+        //производим попытку загрузки переиспользуемой ячейки
+        guard var cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") else {
+            print("Создаем новую ячейку для строки с индексом - \(indexPath.row)")
+            var newCell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+            //конфигурируем ячейку
+            var configuration = newCell.defaultContentConfiguration()
+            configuration.text = "Строка \(indexPath.row)"
+            newCell.contentConfiguration = configuration
+            configure(cell: &newCell, for: indexPath)
+            return newCell
+        }
+    
+        print("Используем старую ячейку для строки с индексом - \(indexPath.row)")
+        configure(cell: &cell, for: indexPath)
+        return cell
+    }
+    private func configure(cell: inout UITableViewCell, for indexPath: IndexPath){
         var configuration = cell.defaultContentConfiguration()
         configuration.text = "Строка \(indexPath.row)"
         cell.contentConfiguration = configuration
-        
-        //возвращаем сконфигурированный экземпляр ячейки
-        return cell
     }
     
     
